8668d02f6e779664e133f08dc211ffa4d463517f
dnarkiewicz <dnarkiewicz@ctacorp.com>
2014-03-12 15:36:57 -0400

    make this actually work

--- bin/auto_test_changes.sh
+++ bin/auto_test_changes.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 fswatch src/:tests/ '
 echo -n -e "\033]0; ? Test \007"; 
-T=$(phpunit tests/);
-if [ $(grep --quiet FAILURE <<< $T) ] || [ $(grep --quiet Error <<< $T) ]; then 
+T=$(phpunit tests/LiveTest);
+grep --quiet FAILURE <<< $T;
+F="${?}";
+grep --quiet "Fatal Error" <<< $T;
+E="${?}"
+if [ $F -eq "0" ] || [ $E -eq "0" ]; then 
     echo -n -e "\033]0; 💩  Fail \007"; 
 else 
     echo -n -e "\033]0; 👍  Pass \007"; 
 fi;
 echo -e "$T";
 ';
+#if [ -n $(grep --quiet FAILURE <<< $T) ] || [ -n $(grep --quiet "Fatal Error" <<< $T) ]; then 
 #fswatch src/ "composer install"
 #    echo -n -e "\033]0; ✗ Fail \007"; 
 #    echo -n -e "\033]0; ✓ Pass \007"; 
