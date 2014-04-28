27a2cf58a84ca5218356d9abe54dac42c4822d0e
dnarkiewicz <dnarkiewicz@ctacorp.com>
2014-04-28 14:40:04 -0400

    dont push docs to drupal

--- bin/push_to_drupal_module.sh
+++ bin/push_to_drupal_module.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cp dist/lib/*.lib.php  ~/Sites/drupal-synd/sites/all/modules/syndicated_content/lib/
-cp -rp docs            ~/Sites/drupal-synd/sites/all/modules/syndicated_content/lib/docs
+#cp -r docs             ~/Sites/drupal-synd/sites/all/modules/syndicated_content/lib/docs
+#chmod -R 775           ~/Sites/drupal-synd/sites/all/modules/syndicated_content/lib/docs
 echo "Syndication API moved into drupal module (clobber-mode)"
